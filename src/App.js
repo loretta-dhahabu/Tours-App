5ef389fb31f9ba4ae533daacd00d73d9759b4652
loretta-dhahabu <loreydjefwa@gmail.com>
2022-07-24 16:25:30 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import { Button,  } from "react-bootstrap";
+import  Button from "react-bootstrap/Button";
+function App() {
+  return (
+    <>
+      <div>
+        <Button variant="primary">Good afternoon</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
